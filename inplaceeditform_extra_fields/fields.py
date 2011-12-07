7d77d7b77476a83f8536e1910d288230aff4187a
Pablo Martín <goinnn@gmail.com>
2011-12-07 17:24:41 +0100

    Adding autocomplete field

---
+++ inplaceeditform_extra_fields/fields.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.template.loader import render_to_string
+
+from ajax_select.fields import AutoCompleteSelectMultipleField, AutoCompleteSelectField
+
+from inplaceeditform.fields import AdaptorForeingKeyField, AdaptorCommaSeparatedManyToManyField
+from ajax_select import make_channel
+
+
+class AdaptorAutoCompleteProvider(object):
+
+    def get_field(self):
+        field = super(AdaptorAutoCompleteProvider, self).get_field()
+        lookup = self.config.get('lookup', None)
+        if lookup:
+            field.field = self.auto_complete_field(lookup, required=field.field.required)
+        return field
+
+    def render_media_field(self, template_name="inplaceeditform_extra_fields/autocomplete/render_media_field.html", extra_context=None):
+        return super(AdaptorAutoCompleteProvider, self).render_media_field(template_name, extra_context)
+
+
+    def render_value_edit(self):
+        value = super(AdaptorAutoCompleteProvider, self).render_value_edit()
+        return render_to_string('inplaceeditform_extra_fields/autocomplete/render_value.html', {'value': value,
+                                                                                                'MEDIA_URL': settings.MEDIA_URL,
+                                                                                                'is_ajax': self.request.is_ajax()})
+
+
+class AdaptorAutoCompleteForeingKeyField(AdaptorAutoCompleteProvider, AdaptorForeingKeyField):
+
+    auto_complete_field = AutoCompleteSelectField
+
+    @property
+    def name(self):
+        return 'auto_fk'
+
+
+class AdaptorAutoCompleteManyToManyField(AdaptorAutoCompleteProvider, AdaptorCommaSeparatedManyToManyField):
+
+    auto_complete_field = AutoCompleteSelectMultipleField
+
+    @property
+    def name(self):
+        return 'auto_m2m'
+
+    def get_value_editor(self, value):
+        return [pk for pk in value.split("|") if pk]
