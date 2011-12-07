ba4323551cc480989dbf8a6586cc62b2fb03d95f
Pablo Martín <goinnn@gmail.com>
2011-12-07 18:03:09 +0100

    more robust the app

--- inplaceeditform_extra_fields/fields.py
+++ inplaceeditform_extra_fields/fields.py
@@ -1,36 +1,54 @@
 from django.conf import settings
 from django.template.loader import render_to_string
 
-from ajax_select.fields import AutoCompleteSelectMultipleField, AutoCompleteSelectField
 from inplaceeditform.fields import AdaptorForeingKeyField, AdaptorCommaSeparatedManyToManyField
 
 
 class AdaptorAutoCompleteProvider(object):
 
+    def install_ajax_select(self):
+        if 'ajax_select' in settings.INSTALLED_APPS:
+            lookup = self.config.get('lookup', None)
+            auto_field = self.auto_complete_field
+            if lookup and auto_field:
+                return True
+        return False
+
     def get_field(self):
         field = super(AdaptorAutoCompleteProvider, self).get_field()
-        lookup = self.config.get('lookup', None)
-        if lookup:
-            field.field = self.auto_complete_field(lookup, required=field.field.required)
+        if self.install_ajax_select():
+            lookup = self.config.get('lookup', None)
+            auto_field = self.auto_complete_field
+            field.field = auto_field(lookup, required=field.field.required)
         return field
 
     def render_media_field(self,
             template_name="inplaceeditform_extra_fields/autocomplete/render_media_field.html",
             extra_context=None):
-        return super(AdaptorAutoCompleteProvider, self).render_media_field(template_name, extra_context)
+        if self.install_ajax_select():
+            return super(AdaptorAutoCompleteProvider, self).render_media_field(template_name, extra_context)
+        return super(AdaptorAutoCompleteProvider, self).render_media_field()
 
 
     def render_value_edit(self):
         value = super(AdaptorAutoCompleteProvider, self).render_value_edit()
-        return render_to_string('inplaceeditform_extra_fields/autocomplete/render_value.html',
-                                {'value': value,
-                                 'MEDIA_URL': settings.MEDIA_URL,
-                                 'is_ajax': self.request.is_ajax()})
+        if self.install_ajax_select():
+            return render_to_string('inplaceeditform_extra_fields/autocomplete/render_value.html',
+                                    {'value': value,
+                                    'MEDIA_URL': settings.MEDIA_URL,
+                                    'is_ajax': self.request.is_ajax()})
+        return super(AdaptorAutoCompleteProvider, self).render_value_edit()
 
 
 class AdaptorAutoCompleteForeingKeyField(AdaptorAutoCompleteProvider, AdaptorForeingKeyField):
 
-    auto_complete_field = AutoCompleteSelectField
+    @property
+    def auto_complete_field(self):
+        try:
+            from ajax_select.fields import AutoCompleteSelectField
+            return AutoCompleteSelectField
+        except ImportError:
+            return None
 
     @property
     def name(self):
@@ -39,7 +57,13 @@ class AdaptorAutoCompleteForeingKeyField(AdaptorAutoCompleteProvider, AdaptorFor
 
 class AdaptorAutoCompleteManyToManyField(AdaptorAutoCompleteProvider, AdaptorCommaSeparatedManyToManyField):
 
-    auto_complete_field = AutoCompleteSelectMultipleField
+    @property
+    def auto_complete_field(self):
+        try:
+            from ajax_select.fields import AutoCompleteSelectMultipleField
+            return AutoCompleteSelectMultipleField
+        except ImportError:
+            return None
 
     @property
     def name(self):
---
+++ inplaceeditform_extra_fields/settings.py
@@ -0,0 +1,2 @@
+ADAPTOR_INPLACEEDIT = {'auto_fk': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteForeingKeyField',
+                       'auto_m2m': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteManyToManyField'}
