5d1d02c478a23fc72eecb3f21a3049b42cecc722
Pablo Martín <goinnn@gmail.com>
2013-10-04 20:28:50 +0200

    Update the release version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name="django-inplaceedit-extra-fields",
-    version="0.2.3",
+    version="0.2.4",
     author="Pablo Martin",
     author_email="goinnn@gmail.com",
     description="Django application that adds other useful fields to Django inplace edit",
