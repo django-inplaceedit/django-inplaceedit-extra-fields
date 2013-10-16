6593e017f7c1663840558edfd313223eeaf1a6dc
Pablo Martín <goinnn@gmail.com>
2013-10-16 12:34:36 +0200

    Add the new adaptor to adaptors file

--- inplaceeditform_extra_fields/adaptors.py
+++ inplaceeditform_extra_fields/adaptors.py
@@ -18,4 +18,5 @@
 ADAPTOR_INPLACEEDIT = {'auto_fk': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteForeingKeyField',
                        'auto_m2m': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteManyToManyField',
                        'image_thumb': 'inplaceeditform_extra_fields.fields.AdaptorImageThumbnailField',
-                       'tiny': 'inplaceeditform_extra_fields.fields.AdaptorTinyMCEField'}
+                       'tiny': 'inplaceeditform_extra_fields.fields.AdaptorTinyMCEField',
+                       'tiny_simple': 'inplaceeditform_extra_fields.fields.AdaptorSimpleTinyMCEField'}
